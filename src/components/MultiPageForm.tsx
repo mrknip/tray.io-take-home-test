8ceb4aaf37e6667a293c09d5970b28250777df08
Drew Johnson <drew.johnson@mangahigh.com>
2021-07-04 16:17:36 +0100

    Add basic docblocks to MultiPageForm

--- src/components/MultiPageForm.tsx
+++ src/components/MultiPageForm.tsx
@@ -10,14 +10,24 @@ import { ValidatorMap } from '../types';
 import ProgressIndicator from './ProgressIndicator';
 
 interface FormPageData {
+  /** React component, forms the display of the page, houses the form inputs.  Must implement interface FormPageProps */
   display: any; // I struggled with the typing for React components throughout this exercise
+  /** The page name - used in the progress display */
   name: string;
+  /** The starting values for fields in each page */
   startingValues?: Record<string, any>;
+  /**
+   * A map of validators for each field.
+   * Validators availale in src/helpers/formValidators - they return an error message or null
+   * Processed in order, so (e.g.) for required fields isNotEmpty normally comes first
+   * */
   validators?: ValidatorMap;
 }
 
 interface MultiPageFormProps {
+  /** An array of config for each page of the form */
   pages: FormPageData[];
+  /** Handler for submission of the complete form, once all pages complete */
   onSubmit: (formData: Record<string, any>) => void;
 }
 
